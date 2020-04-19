f4f9f71aeb94cdb248ba1136e247816973459c86
tiantian <1500846601@qq.com>
2020-04-19 16:08:25 +0800

    使用 chai-spies 模拟点击

--- app.js
+++ app.js
@@ -18,7 +18,9 @@ new Vue({
 // 单元测试
 
 import chai from "chai"
+import spies from "chai-spies"
 
+chai.use(spies)
 const expect = chai.expect
 {
   const Constructor = Vue.extend(Button)
@@ -92,9 +94,12 @@ const expect = chai.expect
     }
   })
   vm.$mount()
-  vm.$on("click", function () {
-    expect(1).to.eq(1)
+
+  let spy = chai.spy(function () {
   })
+
+  vm.$on("click", spy)
   let button = vm.$el
   button.click()
+  expect(spy).to.have.been.called()
 }
