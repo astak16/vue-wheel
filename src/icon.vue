9cc5ebe3b5e82996c406dfc950cbb98a82cec69b
tiantian <1500846601@qq.com>
2020-04-19 14:43:38 +0800

    新建组件 icon

---
+++ src/icon.vue
@@ -0,0 +1,19 @@
+<template>
+  <svg class="g-icon">
+    <use :xlink:href="`#i-${name}`"></use>
+  </svg>
+</template>
+
+<script>
+  export default {
+    name: "icon",
+    props:["name"]
+  }
+</script>
+
+<style scoped>
+  .g-icon {
+    height: 1em;
+    width: 1em;
+  }
+</style>
