921b15ac510300879e85e1e91890f30b4846f86f
tiantian <1500846601@qq.com>
2020-04-19 14:55:35 +0800

    添加loading

--- src/button.vue
+++ src/button.vue
@@ -1,6 +1,7 @@
 <template>
   <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
     <g-icon v-if="icon" class="icon" :name="icon"></g-icon>
+    <g-icon class="loading" name="loading"></g-icon>
     <div class="content">
       <slot></slot>
     </div>
@@ -69,6 +70,19 @@
         order: 2;
         margin-right: 0;
         margin-left: .1em;
+      }
+    }
+
+    > .loading {
+      animation: spin 2s infinite linear;
+    }
+
+    @keyframes spin {
+      0% {
+        transform: rotate(0deg);
+      }
+      100% {
+        transform: rotate(360deg);
       }
     }
   }
