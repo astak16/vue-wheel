9b031adea1b4d092466e3b45090315866236426f
tiantian <1500846601@qq.com>
2020-04-19 15:26:37 +0800

    解决 button-group 第一层元素不是 button 的问题

--- src/g-button-group.vue
+++ src/g-button-group.vue
@@ -6,7 +6,16 @@
 
 <script>
   export default {
-    name: "g-button-group"
+    name: "g-button-group",
+    mounted() {
+      for(let node of this.$el.children){
+        const name = node.nodeName.toLowerCase()
+        if(name!== "button"){
+          console.error(`g-button-group 的子元素应该全是 g-button，但是你写的是：${name}`)
+        }
+      }
+
+    }
   }
 </script>
 
