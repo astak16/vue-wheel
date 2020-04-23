83532a0e1c7fad010132b80ea439c546a77a6d40
tiantian <1500846601@qq.com>
2020-04-23 23:02:35 +0800

    布局组件完成

---
+++ src/footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="footer">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "footer1"
+  }
+</script>
+
+<style scoped lang="scss">
+.footer{
+
+}
+</style>
---
+++ src/header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="header">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "header1"
+  }
+</script>
+
+<style scoped lang="scss">
+.header{
+
+}
+</style>
