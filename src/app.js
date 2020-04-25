dcc30be60fd2561dfb6bd41c3eefe90c340ce695
tiantian <1500846601@qq.com>
2020-04-25 22:00:39 +0800

    toast 完成

--- src/app.js
+++ src/app.js
@@ -1,29 +1,29 @@
 import Vue from "vue"
-import Button from "./button"
-import Icon from "./icon"
-import ButtonGroup from "./g-button-group"
-import Input from "./input"
-import Row from "./row"
-import Col from "./col"
-import Header from "./header"
-import Content from "./content"
-import Footer from "./footer"
-import Sider from "./sider"
-import Layout from "./layout"
-import plugin from "./plugin";
+// import Button from "./button"
+// import Icon from "./icon"
+// import ButtonGroup from "./g-button-group"
+// import Input from "./input"
+// import Row from "./row"
+// import Col from "./col"
+// import Header from "./header"
+// import Content from "./content"
+// import Footer from "./footer"
+// import Sider from "./sider"
+// import Layout from "./layout"
 import Toast from "./toast"
+import plugin from "./plugin";
 
-Vue.component("g-button", Button)
-Vue.component("g-icon", Icon)
-Vue.component("g-button-group", ButtonGroup)
-Vue.component("g-input", Input)
-Vue.component("g-row", Row)
-Vue.component("g-col", Col)
-Vue.component("g-header", Header)
-Vue.component("g-content", Content)
-Vue.component("g-footer", Footer)
-Vue.component("g-sider", Sider)
-Vue.component("g-layout", Layout)
+// Vue.component("g-button", Button)
+// Vue.component("g-icon", Icon)
+// Vue.component("g-button-group", ButtonGroup)
+// Vue.component("g-input", Input)
+// Vue.component("g-row", Row)
+// Vue.component("g-col", Col)
+// Vue.component("g-header", Header)
+// Vue.component("g-content", Content)
+// Vue.component("g-footer", Footer)
+// Vue.component("g-sider", Sider)
+// Vue.component("g-layout", Layout)
 Vue.component("g-toast", Toast)
 Vue.use(plugin)
 new Vue({
@@ -41,18 +41,27 @@ new Vue({
     inputChange(e) {
       console.log(e)
     },
-    showToast() {
+    showToast1(){
+      this.showToast("top")
+    },
+    showToast2(){
+      this.showToast("middle")
+    },
+    showToast3(){
+      this.showToast("bottom")
+    },
+    showToast(position) {
       this.$toast("你的智商需要充值", {
-        position: "bottom",
-        // enableHtml: false,
+        position,
+        enableHtml: false,
         closeButton: {
           text: "已充值",
           callback() {
             console.log("他说已经充值了")
           }
         },
-        autoClose: true,
-        autoCloseDelay: 3
+        autoClose: 3,
+        // autoCloseDelay: 3
       })
     }
   }
