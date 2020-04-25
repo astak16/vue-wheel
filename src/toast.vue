5de8b2f9e14db401a6e54e56305b9e388c3d74d1
tiantian <1500846601@qq.com>
2020-04-25 21:20:43 +0800

    增加样式

--- src/toast.vue
+++ src/toast.vue
@@ -70,12 +70,13 @@
       },
       close() {
         this.$el.remove()
+        this.$emit("close")
         this.$destroy()
       },
       onClickClose() {
         this.close()
         if (this.closeButton && typeof this.closeButton.callback === "function") {
-          this.closeButton.callback()
+          this.closeButton.callback(this)
         }
       }
     }
@@ -86,7 +87,13 @@
   $font-size: 14px;
   $toast-min-height: 40px;
   $toast-bg: rgba(0, 0, 0, .75);
+  @keyframes fade-in {
+    0% {opacity: 0; transform: translateY(100%);}
+    100% {opacity: 1; transform: translateY(0);}
+  }
+
   .toast {
+    animation: fade-in 1s;
     font-size: $font-size;
     height: $toast-min-height;
     line-height: 1.8;
