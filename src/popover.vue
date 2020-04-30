e5daf38fc469dfcf8a184dcf6571da5dadc03c06
tiantian <1500846601@qq.com>
2020-04-30 22:09:24 +0800

    4个方向的按钮

--- src/popover.vue
+++ src/popover.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="popover" @click="onClick" ref="popover">
-    <div class="content-wrap" v-if="visible" ref="contentWrapper">
+    <div class="content-wrap" v-if="visible" ref="contentWrapper"
+         :class="{[`position-${position}`]:true}">
       <slot name="content"></slot>
     </div>
     <span ref="triggerWrapper">
@@ -12,6 +13,16 @@
 <script>
   export default {
     name: "popover",
+    props: {
+      position: {
+        type: String,
+        default: "top",
+        validator(value) {
+          console.log(value)
+          return ['top', 'bottom', 'left', 'right'].indexOf(value) >= 0
+        }
+      }
+    },
     data() {
       return {
         visible: false
@@ -22,10 +33,26 @@
     },
     methods: {
       positionContent() {
-        document.body.appendChild(this.$refs.contentWrapper)
-        const {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
-        this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
-        this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
+        const {contentWrapper, triggerWrapper} = this.$refs
+        document.body.appendChild(contentWrapper)
+        const {width, height, top, left} = triggerWrapper.getBoundingClientRect()
+        console.log(width, height, top)
+        if (this.position === "top") {
+          contentWrapper.style.left = left + window.scrollX + 'px'
+          contentWrapper.style.top = top + window.scrollY + 'px'
+        } else if (this.position === "bottom") {
+          contentWrapper.style.left = left + window.scrollX + 'px'
+          contentWrapper.style.top = top + height + window.scrollY + 'px'
+        } else if (this.position === "left") {
+          contentWrapper.style.left = left + window.scrollX + 'px'
+          const {height: height2} = contentWrapper.getBoundingClientRect()
+          contentWrapper.style.top = top + window.scrollY + (height2 - height) / 2 + 'px'
+        } else if (this.position === "right") {
+          contentWrapper.style.left = left + width + window.scrollX + 'px'
+          const {height: height2} = contentWrapper.getBoundingClientRect()
+          contentWrapper.style.top = top + window.scrollY + (height2 - height) / 2 + 'px'
+        }
+
       },
       onClickDocument(e) {
         if (this.$refs.popover &&
@@ -90,17 +117,82 @@
       width: 0;
       height: 0;
       position: absolute;
-      left: 10px;
     }
 
-    &::before {
-      border-top-color: black;
-      top: 100%;
+    &.position-top {
+      transform: translateY(-100%);
+      margin-top: -16px;
+
+      &::before, &::after {
+        left: 10px;
+      }
+
+      &::before {
+        border-top-color: black;
+        top: 100%;
+      }
+
+      &::after {
+        border-top-color: white;
+        top: calc(100% - 1px);
+      }
     }
 
-    &::after {
-      border-top-color: white;
-      top: calc(100% - 1px);
+    &.position-bottom {
+      margin-top: 10px;
+
+      &::before, &::after {
+        left: 10px;
+      }
+
+      &::before {
+        border-bottom-color: black;
+        bottom: 100%;
+      }
+
+      &::after {
+        border-bottom-color: white;
+        bottom: calc(100% - 1px);
+      }
+    }
+
+    &.position-left {
+      transform: translateX(-100%);
+      margin-left: -10px;
+
+      &::before, &::after {
+        transform: translateY(-50%);
+        top: 50%;
+      }
+
+      &::before {
+        border-left-color: black;
+        left: 100%;
+      }
+
+      &::after {
+        border-left-color: white;
+        left: calc(100% - 1px);
+      }
+    }
+
+    &.position-right {
+      margin-left: 10px;
+
+      &::before, &::after {
+        transform: translateY(-50%);
+        top: 50%;
+      }
+
+      &::before {
+        border-right-color: black;
+        right: 100%;
+      }
+
+      &::after {
+        border-right-color: white;
+        right: calc(100% - 1px);
+      }
     }
   }
 
